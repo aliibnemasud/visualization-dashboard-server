3051c803cf12d9ea3f7a77b3d3bc56be49e34d46
aliibnemasud <mdmasudrony@gmail.com>
2022-09-25 03:38:35 +0600

    Server ready

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+require('dotenv').config();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('visualization Dashboard Server is Running......')
+})
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://visualization-ashboard:CI1nunRmFEiE6B64@visualization-dashboard.aatcaul.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+const run = async () => {
+    
+    try {
+
+        await client.connect();
+        const businessDataCollection = client.db('visualization-dashboard').collection('businessdata');
+
+         // Load all data
+
+         app.get('/businessdata', async (req, res) => {                        
+            const query = {};
+            const cursor = businessDataCollection.find(query);
+            const data = await cursor.toArray();
+            res.send(data);
+        })
+
+    } 
+    
+    finally {
+        // console.close()
+    }
+}
+
+run().catch(console.dir);
+
+
+
+app.listen(port, () => {
+    console.log(`visualization Dashboard listening on port ${port}`)
+})
