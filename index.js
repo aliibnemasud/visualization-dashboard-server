dab9cbdc6cb985fb15cbebc1747b1c81e5017ce0
aliibnemasud <mdmasudrony@gmail.com>
2022-09-25 22:03:00 +0600

    admin part added

--- index.js
+++ index.js
@@ -12,25 +12,88 @@ app.get('/', (req, res) => {
 })
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER_NAME }:${process.env.DB_PASS}@visualization-dashboard.aatcaul.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASS}@visualization-dashboard.aatcaul.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const run = async () => {
-    
+
     try {
         await client.connect();
         const businessDataCollection = client.db('visualization-dashboard').collection('businessdata');
-         // Load all data
+        const formCollection = client.db('visualization-dashboard').collection('formData');
+        const userCollection = client.db('visualization-dashboard').collection('users');
+        const allUsersCollection = client.db('visualization-dashboard').collection('allUsers');
 
-         app.get('/businessdata', async (req, res) => {                        
+        // Load all data
+
+        app.get('/businessdata', async (req, res) => {
             const query = {};
             const cursor = businessDataCollection.find(query);
             const data = await cursor.toArray();
             res.send(data);
         })
 
-    } 
-    
+        // Posting data for user portal
+
+        app.post('/addFrom', async (req, res) => {
+            const formData = req.body;
+            console.log()
+            const result = await formCollection.insertOne(formData);
+            res.send(result);
+        })
+
+        // All users
+
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const cursor = userCollection.find(query);
+            const data = await cursor.toArray();
+            res.send(data)
+        })
+
+        // Form Data
+
+        app.get('/formdata', async (req, res) => {
+            const query = {};
+            const cursor = formCollection.find(query);
+            const data = await cursor.toArray();
+            res.send(data)
+        })
+
+        // registering user to the database
+
+        /*   app.put('/users/:email', async (req, res) => {
+              const email = req.params.email;
+              const user = req.body;
+              const filter = { email: email };
+              const option = { upsert: true };
+              const updateUser = {
+                  $set: user,
+              };
+              const result = await usersCollection.updateOne(filter, updateUser, option);
+              res.send(result);
+          })
+   */
+        // Posting User to the database
+
+        app.post('/users', async (req, res) => {
+            const users = req.body;
+            const result = await allUsersCollection.insertOne(users);
+            res.send(result);
+        })
+
+        // Get admin
+        
+        app.get('/user/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await allUsersCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin });
+        })
+
+
+    }
+
     finally {
         // console.close()
     }
